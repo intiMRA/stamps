2e4af9d75e384778fc035617175def8f50dfed68
inti <inti_resende_albuquerque@bnz.co.nz>
2021-05-03 20:08:56 +1200

    Added encryption

--- Stamps/Stamps/login/LogInViewModule.swift
+++ Stamps/Stamps/login/LogInViewModule.swift
@@ -12,6 +12,27 @@ class LogInViewModule: ObservableObject {
     @Published var logInSuccess = false
     
     func login() {
-        logInSuccess = true
+        guard let s = try? encryptMessage(message: username, encryptionKey: password), let ss = try? decryptMessage(encryptedMessage: s, encryptionKey: password) else {
+            return
+        }
+        
+        if ss == username {
+            logInSuccess = true
+        }
+    }
+    
+    func encryptMessage(message: String, encryptionKey: String) throws -> String {
+        let messageData = message.data(using: .utf8)!
+        let cipherData = RNCryptor.encrypt(data: messageData, withPassword: encryptionKey)
+        return cipherData.base64EncodedString()
+    }
+    
+    func decryptMessage(encryptedMessage: String, encryptionKey: String) throws -> String {
+        
+        let encryptedData = Data.init(base64Encoded: encryptedMessage)!
+        let decryptedData = try RNCryptor.decrypt(data: encryptedData, withPassword: encryptionKey)
+        let decryptedString = String(data: decryptedData, encoding: .utf8)!
+        
+        return decryptedString
     }
 }
