8463a64708112157e3c4ec6117be07ba33d60f67
inti <inti.resende@gmail.com>
2021-05-30 18:02:30 +1200

    uploading cards

--- Stamps/Stamps/Scanning/ScanningViewModel.swift
+++ Stamps/Stamps/Scanning/ScanningViewModel.swift
@@ -24,22 +24,23 @@ class ScanningViewModel: ObservableObject {
     }
     
     func fetchStoreDetails() -> StoreModel? {
-        return StoreModel()
+        return StoreModel(storeName: "The Store", storeId: "bTvMM0eKsth2n6GMvoawZdlLWV72")
     }
-    
     
     func foundQRCode(_ code: String) {
         guard !code.isEmpty else {
             return
         }
         
-        guard let store = fetchStoreDetails() else {
-            return
-        }
         if let card = ReduxStore.shared.customerModel?.stampCards.first(where: { $0.storeName == code }) {
-            ReduxStore.shared.changeState(customerModel: ReduxStore.shared.customerModel?.replaceCard(card), storeModel: nil)
+            ReduxStore.shared.changeState(customerModel: ReduxStore.shared.customerModel?.replaceCard(card.stamp()))
             cardApi.saveCard(card)
         } else {
+            
+            guard let store = fetchStoreDetails() else {
+                return
+            }
+            
             let card = CardData.newCard(storeName: store.storeName, storeId: store.storeId, listIndex: ReduxStore.shared.customerModel?.stampCards.count)
             ReduxStore.shared.addCard(card)
             cardApi.saveCard(card)
