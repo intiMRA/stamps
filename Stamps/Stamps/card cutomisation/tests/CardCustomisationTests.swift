f6ae07a99d095eba753097305cf8cc492dac7f82
inti <inti.resende@gmail.com>
2021-08-09 23:06:16 +1200

    addedncustomization test file

---
+++ Stamps/Stamps/card cutomisation/tests/CardCustomisationTests.swift
@@ -0,0 +1,14 @@
+//
+//  CardCustomisationTests.swift
+//  StampsTests
+//
+//  Created by Inti Albuquerque on 9/08/21.
+//
+
+import XCTest
+@testable import Stamps
+
+class CardCustomisationTests: XCTestCase {
+    
+
+}
